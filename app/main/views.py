9c480425900c75885815f902934aabc099b452fc
iter8 <randomdweller.me@gmail.com>
2022-04-15 03:17:47 +0530

    Front 1.0

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from . import main
 
-@main.route('/')
-def index():
-    return render_template('index.html')
+
+@main.route('/', methods = ['GET', 'POST'])
+def form():
+    f = open("test.txt","a+")
+    if request.method == 'POST':
+        content = request.form['content']
+        try:
+            f.write(content + "\n")
+            return redirect('/')
+        except:
+            return "There was an issue adding the text. "
+
+    else:
+        f = open("test.txt","r")
+        tasklist = f.readlines()
+        
+        return render_template("index.html", tasklist = tasklist)
+
