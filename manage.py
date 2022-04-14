2963f6126782b6ba176a5d17027740670ad20a4e
Abhishek <abhishek.pandey.3237@gmail.com>
2022-04-14 10:08:55 +0530

    Added scripts for running the application

--- manage.py
+++ manage.py
@@ -1,6 +1,4 @@
 import os
 from app import create_app
 
-print(os.getenv("FLASK_CONFIG", "default"))
-
 app = create_app(os.getenv("FLASK_CONFIG", "default"))
---
+++ run.bat
@@ -0,0 +1,5 @@
+set FLASK_APP=manage.py
+set FLASK_DEBUG=1
+set FLASK_CONFIG=development
+
+flask run
