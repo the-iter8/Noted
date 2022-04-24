e1b1b9e49d3f953cc2753d674f3e559399ca4ce9
Abhishek <abhishek.pandey.3237@gmail.com>
2022-04-24 07:52:04 +0530

    added functionality for delete button

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, abort
 
 from .forms import ListForm
 from . import main
@@ -6,9 +6,12 @@ from .. import db
 from ..models import User, Note
 
 @main.route('/', methods = ['GET', 'POST'])
-def form_():
+def home():
     form = ListForm()
-    if request.method == 'POST':
+    if request.method == 'GET':
+        tasklist = [note for note in Note.query.all()]
+        return render_template('index.html', form=form, tasklist=tasklist)
+    elif request.method == 'POST':
         if form.validate_on_submit():
             content = form.content.data
             # TODO: Currently only working for one User.
@@ -19,7 +22,20 @@ def form_():
             db.session.add(note)
             db.session.commit()
             return redirect('/')
-    elif request.method == 'GET':
-        tasklist = [note.title for note in Note.query.all()]
-        return render_template('index.html', form=form, tasklist=tasklist)
 
+@main.route('/delete/<int:note_id>', methods = ['GET','POST'])
+def delete(note_id):
+    if request.method == 'GET':
+        return abort(404)
+    elif request.method == 'POST':
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        return redirect('/')
+
+@main.route('/edit/<int:note_id>', methods = ['GET', 'POST'])
+def edit(note_id):
+    if request.method == 'GET':
+        return abort(404)
+    elif request.method == 'POST':
+        return redirect('/')
